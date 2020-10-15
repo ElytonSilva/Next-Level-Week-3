36935740c21c2d711eec173a64905b317d7a1c88
Elyton <elyton.moreira163@gmail.com>
2020-10-15 19:02:20 -0400

    Atualização views

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,3 +1,5 @@
+
+
 //create map
 const map = L.map('mapid').setView([2.8247286,-60.6791095], 16);
 
@@ -8,22 +10,40 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
 
 //create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
+    iconUrl: "/images/map-marker.svg",
     iconSize: [58, 68],
     iconAnchor: [29,68],
     popAnchor: [170, 2]
 })
 
-// create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: "map-popup",
-    minWidth: 240,
-    minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>')
+function addMaker({id, name, lat, lng}){
 
-//create and add marker
-L
-.marker([2.8247286,-60.6791095], { icon })
-.addTo(map)
-.bindPopup(popup)
+    // create popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: "map-popup",
+        minWidth: 240,
+        minHeight: 240
+    }).setContent(`${name}<a href="/orphanage?id=${id}"> <img src="/images/arrow-white.svg"> </a>`)
+
+    //create and add marker
+    L
+    .marker([lat, lng], { icon })
+    .addTo(map)
+    .bindPopup(popup)
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span')
+orphanagesSpan.forEach( span =>{
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng
+    }
+    addMaker(orphanage)
+})
+
+
+//console.log(orphanagesSpan)
+
